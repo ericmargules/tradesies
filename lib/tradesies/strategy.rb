16bcd1fb4fd0db6907548f2ed47ba67ced7c995d
Eric Margules <eric.i.margules@gmail.com>
2017-10-17 19:15:36 -0500

    add sma, ema, cci consume action to strategy

--- lib/tradesies/strategy.rb
+++ lib/tradesies/strategy.rb
@@ -2,22 +2,39 @@ require_relative 'logger'
 
 module Tradesies
 	class Strategy
-
+		attr_reader :prices, :trades, :candlesticks, :smas, :emas, :ccis
+		
 		def initialize
 			@output = Logger.new
-			@prices = []
-			@closes = []
+			@indicator = Indicator.new
 			@trades = []
+			@prices = []
 			@current_price = ""
-			@current_close = ""
-			@allowable_trades = 1
-			@indicators = ""
+			@max_trades = 1
+
+			# Here be indicators
+			@candlesticks = []
+			@smas = []
+			@emas = []
+			@ccis = []
 		end
 
 		def consume(candlestick)
+			@candlesticks << candlestick.select{|k,v| /high|low|close/.match(k) }
 			@current_price = candlestick["weightedAverage"]
 			@prices << @current_price
+
+			decode(candlestick) if @prices.length >= 24
+
 			@output.log("Price: #{@current_price}")
+		end
+
+		private
+
+		def decode(candlestick)
+			@smas << @indicator.sma(@prices, 24)
+			@emas << @indicator.ema(@prices, 4)
+			@ccis << @indicator.cci(@candlesticks, 20)
 		end
 	end
 end
