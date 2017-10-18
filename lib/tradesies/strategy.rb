42b30a5e21a1a7049a0cc0c786c05dd262a1222c
Eric Margules <eric.i.margules@gmail.com>
2017-10-17 20:10:20 -0500

    add trading logic to strategy

--- lib/tradesies/strategy.rb
+++ lib/tradesies/strategy.rb
@@ -3,17 +3,17 @@ require_relative 'logger'
 module Tradesies
 	class Strategy
 		attr_reader :prices, :trades, :candlesticks, :smas, :emas, :ccis
-		
+
 		def initialize
 			@output = Logger.new
 			@indicator = Indicator.new
 			@trades = []
+			@candlesticks = []
 			@prices = []
 			@current_price = ""
 			@max_trades = 1
 
 			# Here be indicators
-			@candlesticks = []
 			@smas = []
 			@emas = []
 			@ccis = []
@@ -24,17 +24,51 @@ module Tradesies
 			@current_price = candlestick["weightedAverage"]
 			@prices << @current_price
 
-			decode(candlestick) if @prices.length >= 24
+			harvest_candlestick if @prices.length >= 24
 
 			@output.log("Price: #{@current_price}")
 		end
 
 		private
 
-		def decode(candlestick)
+		def harvest_candlestick
 			@smas << @indicator.sma(@prices, 24)
 			@emas << @indicator.ema(@prices, 4)
 			@ccis << @indicator.cci(@candlesticks, 20)
+		end
+
+		def eval_positions
+			open_trades = @trades.select{|trade| trade.status == :open }
+			if open_trades.any? 
+				open_trades[-1].sell if eval_sell
+			end
+			if open_trades.length < @max_trades
+				@trades << Trade.new(@current_price) if eval_buy
+			end
+		end
+
+		def eval_buy
+			lower_sma? || (upward_ema? && @ccis[-1] < -100)
+		end
+
+		def eval_sell
+			higher_sma? || (downward_ema && @ccis[-1] > 100)
+		end
+
+		def lower_sma?
+			@sma[-1] < @ema[-1]
+		end
+
+		def higher_sma?
+			@sma[-1] > @ema[-1]
+		end
+
+		def upward_ema?
+			emas[-3] < emas[-2] && emas[-2] < emas[-1]
+		end
+
+		def downward_ema?
+			emas[-1] < emas[-2] && emas[-2] < emas[-3]
 		end
 	end
 end
