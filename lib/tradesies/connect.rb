f3832ee532b22954d7a85ed9dac6d701a5b9e151
Eric Margules <eric.i.margules@gmail.com>
2017-11-12 20:11:50 -0600

    refactor ticker and switch exchange to GDAX

--- lib/tradesies/connect.rb
+++ lib/tradesies/connect.rb
@@ -2,21 +2,38 @@ require 'httparty'
 require_relative 'options'
 
 module Tradesies
-	class KrakenConnect
+	class GDAXConnect
 		attr_reader :options
 
-		def initialize(argv = [], api_key = nil, api_secret = nil, options = {})
+		def initialize(argv = [])
 			@options = Options.new(argv)
-	        @api_key = api_key
-			@api_secret = api_secret
-	    	@api_version = options[:version] ||= '0'
-			@base_uri = options[:base_uri] ||= 'https://api.kraken.com'
+	        @api_key = @options.api_key
+			@api_secret = @options.api_secret
+			@base_url = @options.base_url || "https://api.gdax.com"
 		end
 
-		def get_public(method, opts={})
-			url = @base_uri + '/' + @api_version + '/public/' + method
-			result = HTTParty.get(url, query: opts)
-			result["result"]
+		def get_public(url, opts = {})
+			result = HTTParty.get(url, opts)
+			result.parsed_response
 		end
+
+		def ticker
+			url = "#{@base_url}/products/#{@options.pair}/ticker"
+			get_public(url)
+		end
+
+		def orderbook(opts = {})
+			opts = {query: {"level" => "#{opts}"} } if opts.class == Fixnum
+			url = "#{@base_url}/products/#{options.pair}/book"
+			get_public(url, opts)
+		end
+
+		private
+
+		def log(point)
+			puts "The latest price (#{@options.pair}) is: #{point["price"]}"
+		end
+
 	end
+
 end
--- lib/tradesies/options.rb
+++ lib/tradesies/options.rb
@@ -2,13 +2,16 @@ require 'optparse'
 
 module Tradesies
 	class Options
-		DEFAULT_PAIR = 'XBTUSD'
-		DEFAULT_INTERVAL = '600'
-		attr_reader :pair, :interval
+		DEFAULT_PAIR = 'BTC-USD'
+		DEFAULT_INTERVAL = '300'
+		attr_reader :pair, :interval, :api_key, :api_secret, :base_url
 
 		def initialize(argv)
 			@pair = DEFAULT_PAIR
 			@interval = DEFAULT_INTERVAL
+			@api_key = ""
+			@api_secret = ""
+			@base_url = false
 			parse(argv)
 		end
 		
@@ -23,6 +26,18 @@ module Tradesies
 
 				opts.on("-i", "--interval interval", "Interval for ticker") do |interval|
 					@interval = interval
+				end
+
+				opts.on("-k", "--api_key api_key", "Key for API") do |api_key|
+					@api_key = api_key
+				end
+
+				opts.on("-s", "--api_secret api_secret", "Secret for API") do |api_secret|
+					@api_secret = api_secret
+				end
+
+				opts.on("-u", "--base_url base_url", "Secret for API") do |base_url|
+					@base_url = base_url
 				end
 
 				opts.on('-h', '--help', 'Displays Help') do
