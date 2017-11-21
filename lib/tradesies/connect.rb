9bf04d6e388fd7ee9042c59113b15b1302ed0966
Eric Margules <eric.i.margules@gmail.com>
2017-11-20 22:20:11 -0600

    fix public api @options bug

--- lib/tradesies/connect.rb
+++ lib/tradesies/connect.rb
@@ -6,33 +6,11 @@ require_relative 'options'
 
 module Tradesies
 
-	class CoinbaseExchange
-	    def initialize(key, secret, passphrase)
-	      @key = key
-	      @secret = secret
-	      @passphrase = passphrase
-	    end
-
-	    def signature(request_path='', body='', timestamp=nil, method='GET')
-	      body = body.to_json if body.is_a?(Hash)
-	      timestamp = Time.now.to_i if !timestamp
-
-	      what = "#{timestamp}#{method}#{request_path}#{body}";
-
-	      # create a sha256 hmac with the secret
-	      secret = Base64.decode64(@secret)
-	      hash  = OpenSSL::HMAC.digest('sha256', secret, what)
-	      Base64.strict_encode64(hash)
-	    end
-	end
-
-
 	class GDAXConnect
 		attr_reader :options
 
 		def initialize(argv = [])
 			@options = Options.new(argv)
-			# @base_url = @options.base_url || "https://api.gdax.com"
 		end
 
 		def get_public(url, opts = {})
@@ -68,13 +46,13 @@ module Tradesies
 		# Public API Calls
 
 		def ticker
-			url = "#{@base_url}/products/#{@options.pair}/ticker"
+			url = "#{@options.base_url}/products/#{@options.pair}/ticker"
 			get_public(url)
 		end
 
 		def orderbook(opts = {})
 			opts = {query: {"level" => "#{opts}"} } if opts.class == Fixnum
-			url = "#{@base_url}/products/#{options.pair}/book"
+			url = "#{@options.base_url}/products/#{@options.pair}/book"
 			get_public(url, opts)
 		end
 
